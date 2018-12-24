8adf56566e5b847aef7b4fc180d3bd80c637e13c
Yenzin Choedon <yenzin@yenzins-air.hitronhub.home>
2018-12-24 13:23:13 -0700

    Convert to mvc design pattern

---
+++ Todoey/New Group/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Yenzin Choedon on 2018-12-24.
+//  Copyright © 2018 Tsetan Yeshi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
